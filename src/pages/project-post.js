16e0df9cac3696a6445e794b980c378f490f6013
Nakyeong Lee <nklee.official@gmail.com>
2021-11-07 21:58:21 +0900

    test

--- src/pages/project-post.js
+++ src/pages/project-post.js
@@ -5,7 +5,6 @@ import Layout from "../components/layout"
 import Footer from "../components/footer"
 import SEO from "../components/seo"
 
-import "../assets/css/post.css"
 import "../assets/css/screen.css"
 //내용 뎁스 페이지!
 

7b3457c5e2bdfe56bf4daff4d18f5e0fb69aa552
Nakyeong Lee <nklee.official@gmail.com>
2021-12-01 05:32:37 +0900

    browser test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,11 +2,17 @@
 
 const React = require("react")
 const Layout = require("./src/components/layout")
+const ProjectPost = require("./src/pages/project-post")
 
 exports.onRouteUpdate = () => {
-    if (typeof window !== `undefined`) { window.scrollTo(0, 0)}
-  }
+    // if (typeof window !== `undefined`) { window.scrollTo(0, 0)}
+    window.scrollTo(0, 0)
+}
   
-  exports.shouldUpdateScroll = args => {
-     return false;
-  };
+exports.shouldUpdateScroll = args => {
+    return false;
+};
+
+exports.onClientEntry = () => {
+    console.log("We've started!")
+}

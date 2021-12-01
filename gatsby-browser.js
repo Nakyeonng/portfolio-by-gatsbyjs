f2469fd9b7b9c20ebc7f493f2875b9b7705f1a5a
Nakyeong Lee <nklee.official@gmail.com>
2021-12-01 16:10:41 +0900

    hamburger/route update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,9 @@
 // custom typefaces
 
-const React = require("react")
-const Layout = require("./src/components/layout")
-const ProjectPost = require("./src/pages/project-post")
-
 exports.onRouteUpdate = () => {
     if (typeof window !== `undefined`) { window.scrollTo(0, 0)}
     // window.scrollTo(0, 0)
-    console.log("top!")
+    // console.log("top!")
 }
   
 exports.shouldUpdateScroll = () => {
@@ -16,5 +12,5 @@ exports.shouldUpdateScroll = () => {
 };
 
 exports.onClientEntry = () => {
-    console.log("We've started!")
+    console.log("Welcome!")
 }
